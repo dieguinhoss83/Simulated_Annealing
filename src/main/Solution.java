1ddb5a9a3a1a7ae38eda6c7e911e818ce52510b3
Javier Montero <javier1689@gmail.com>
2015-12-30 14:41:09 +0100

    Bug fix
    Hasta ahora el resultado que indica los nodos donde quedaban las
    estaciones habia que sumarle uno, ahora indica los nodos donde estan
    colocadas las estaciones correctamente

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,11 +1,13 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Solution {
 	public int numeroEstaciones;
-	public String nodosConEstacion;
 	public String nodosCubiertos;
+	public ArrayList<Integer> nodosConEstacion;
 	
-	public Solution(int tamano,String nodosCubiertos,String nodosConEstacion){
+	public Solution(int tamano,String nodosCubiertos,ArrayList<Integer> nodosConEstacion){
 		this.numeroEstaciones = tamano;
 		this.nodosCubiertos = nodosCubiertos;
 		this.nodosConEstacion = nodosConEstacion;
@@ -13,10 +15,17 @@ public class Solution {
 	public int getTamano() {
 		return numeroEstaciones;
 	}
+	public String getNodosConEstacion() {
+		StringBuilder sbStr = new StringBuilder();
+	    for (int i = 0, il = nodosConEstacion.size(); i < il; i++) {
+	        if (i > 0)
+	            sbStr.append(" - ");
+	        
+	        sbStr.append(nodosConEstacion.get(i) + 1);
+	    }
+	    return sbStr.toString();
+	}
 	public String getNodosCubiertos() {
 		return nodosCubiertos;
-	}
-	public String getNodosConEstacion() {
-		return nodosConEstacion;
 	}
 }
